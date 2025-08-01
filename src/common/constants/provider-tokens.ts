33572344fc81de2600b1b4b2e0ef20830fc4ef56
_woo_s.j <sjwoo1999@korea.ac.kr>
2025-08-01 20:33:32 +0900

    refactor: remove unused domain services and interfaces

--- src/common/constants/provider-tokens.ts
+++
@@ -1,8 +0,0 @@
-// Provider Tokens for Dependency Injection
-export const USER_REPOSITORY = 'IUserRepository';
-export const EMOTION_ANALYSIS_REPOSITORY = 'IEmotionAnalysisRepository';
-export const CHAT_REPOSITORY = 'IChatRepository';
-export const USER_SERVICE = 'IUserService';
-export const EMOTION_ANALYSIS_SERVICE = 'IEmotionAnalysisService';
-export const CHAT_SERVICE = 'IChatService';
-export const HISTORY_SERVICE = 'IHistoryService'; 
